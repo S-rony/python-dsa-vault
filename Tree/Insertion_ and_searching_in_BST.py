fcffd7dcd665d2b892dd7b07e6443c54d7a24be0
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-08-22 17:50:21 +0530

    Add files via upload

---
+++ Tree/Insertion_ and_searching_in_BST.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.data = value
+
+def insert(root,value):
+    if root is None:
+        return Node(value)
+    if root.data == value:
+        return root
+    if root.data > value:
+        root.left = insert(root.left,value)
+    else:
+        root.right = insert(root.right,value)
+    return root
+
+def search(root,value):
+    if root is None:
+        print("\nElement Not Found",end = "\n")
+        return
+    if root.data == value:
+        print("Element Found")
+        return
+    if root.data > value:
+        search(root.left,value)
+    else:
+        search(root.right,value)
+
+def InOrder(root):
+    if root is not None:
+        InOrder(root.left)
+        print(root.data, end=" ")
+        InOrder(root.right)
+
+
+root = insert(None,20)
+root = insert(root, 15)
+root = insert(root, 12)
+root = insert(root,18)
+root = insert(root, 30)
+root = insert(root, 40)
+# root = Node(20)
+# root.left = Node(15)
+# root.left.left = Node(12)
+# root.left.right = Node(18)
+# root.right = Node(30)
+# root.right.right = Node(40)
+
+InOrder(root)
+search(root,16)
+search(root,12)
+search(root,30)
