17afb9a3cad6bd193acc1dce504221087d13a291
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-08-24 11:16:43 +0530

    Add files via upload

---
+++ Graph/GraphAdjMatrix.py
@@ -0,0 +1,38 @@
+class Graph:
+    def __init__(self, vertex):
+        self.mat = [[0]*vertex for _ in range(vertex)]
+        self.size = vertex
+
+    def add_edge(self, src, dest):
+        #undirectional edge
+        if (0 <= src < self.size) and (0 <= dest < self.size):
+            self.mat[src][dest] = 1
+            self.mat[dest][src] = 1
+        else:
+            print("Invalid Edge")
+        # directional edge
+        # if (0 <= src < self.size) and (0 <= dest < self.size):
+        #     self.mat[src][dest] = 1
+
+        # weighted edge
+        # if (0 <= src < self.size) and (0 <= dest < self.size):
+        #     self.mat[src][dest] = 3
+
+    def print(self):
+        for row in self.mat:
+            print(" ".join(map(str,row)))
+
+G = Graph(5)
+G.add_edge(0,1)
+G.add_edge(0,2)
+G.add_edge(1,3)
+G.add_edge(2,4)
+G.add_edge(3,4)
+G.add_edge(2,3)
+
+G.print()
+
+
+
+
+
