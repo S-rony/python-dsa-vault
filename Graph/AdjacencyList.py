78811f9e6927f0723124ee727b7a07f506e1b19e
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-08-25 14:14:30 +0530

    Add files via upload

---
+++ Graph/AdjacencyList.py
@@ -0,0 +1,30 @@
+class Graph:
+    def __init__(self):
+        self.adjList = {}
+
+    def add_vertex(self,vertex):
+        if vertex not in self.adjList:
+            self.adjList[vertex] = []
+
+    def add_edge(self, src, dest):
+        self.add_vertex(src)
+        self.add_vertex(dest)
+
+        self.adjList[src].append(dest)
+        self.adjList[dest].append(src)
+
+    def print_graph(self):
+        for vertex in self.adjList:
+            print(f"{vertex } -> { self.adjList[vertex]}")
+
+
+g = Graph()
+g.add_edge(1,2)
+g.add_edge(1,4)
+g.add_edge(2,3)
+g.add_edge(2,4)
+g.add_edge(3,4)
+g.add_edge(3,5)
+g.add_edge(5,4)
+
+g.print_graph()
